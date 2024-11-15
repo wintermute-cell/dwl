d6841f7d6076100744e1bb373c871d7141f814b3
wintermute <kjulius@gmx.de>
2024-11-15 19:47:03 +0100

    fix menus

--- config.def.h
+++ config.def.h
@@ -69,7 +69,7 @@ static const int repeat_delay = 300;
 static const int tap_to_click = 1;
 static const int tap_and_drag = 1;
 static const int drag_lock = 1;
-static const int natural_scrolling = 1;
+static const int natural_scrolling = 0;
 static const int disable_while_typing = 1;
 static const int left_handed = 0;
 static const int middle_button_emulation = 0;
@@ -128,8 +128,8 @@ static const char *menucmd[] = { "fuzzel", NULL };
 // bindsym Print exec grimshot copy area
 // bindsym $mod+Print exec wf-recorder --file "$HOME/Videos/recording_$(date +%d_%m_%y-%H:%M:%S).mp4" -g "$(slurp)" -r 144 -x yuv420p  --audio=alsa_output.usb-Focusrite_Scarlett_Solo_USB-00.HiFi__hw_USB_1__sink.monitor
 // bindsym $mod+Shift+Print exec killall -s SIGINT wf-recorder
-static const char *audiomenu[] = { "~/scripts/desktop_environment/audiomenu.sh", NULL };
-static const char *powermenu[] = { "~/scripts/desktop_environment/powermenu.sh", NULL };
+static const char *audiomenu[] = { "/home/winterveil/scripts/desktop_environment/audiomenu.sh", NULL };
+static const char *powermenu[] = { "/home/winterveil/scripts/desktop_environment/powermenu.sh", NULL };
 static const char *screenshot[] = { "grimshot", "copy", "area", NULL };
 static const char *record[] = { "wf-recorder", "--file", "$HOME/Videos/recording_$(date +%d_%m_%y-%H:%M:%S).mp4", "-g", "$(slurp)", "-r", "144", "-x", "yuv420p", "--audio=alsa_output.usb-Focusrite_Scarlett_Solo_USB-00.HiFi__hw_USB_1__sink.monitor", NULL };
 static const char *stoprecord[] = { "killall", "-s", "SIGINT", "wf-recorder", NULL };
